b468180374fda6e1b9264c143edcfefa1376b226
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-04-21 16:30:46 +0100

    Integrate Coveralls and Simplecov in tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,18 @@
+# frozen_string_literal: true
+
+require 'coveralls'
+Coveralls.wear!
+
+require 'simplecov'
+SimpleCov.start 'rails'
+Rails.application.eager_load!
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -31,6 +40,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
