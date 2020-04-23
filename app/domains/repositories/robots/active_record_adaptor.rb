addc71dd0ddf8f44937ca6855a8b00a3d9414a46
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-04-23 10:52:00 +0100

    Add the ActiveRecordAdaptor to map Robots with AR

--- app/domains/repositories/robots/active_record_adaptor.rb
+++ app/domains/repositories/robots/active_record_adaptor.rb
@@ -3,17 +3,29 @@
 module Repositories
   module Robots
     class ActiveRecordAdaptor < Repositories::Robots::Base
-      def find_by(id:); end
+      def find_by(id:)
+        Robot.find(id)
+      end
 
-      def up(robot, distance); end
+      def up(robot, distance)
+        robot.y += distance
+      end
 
-      def down(robot, distance); end
+      def down(robot, distance)
+        robot.y -= distance
+      end
 
-      def right(robot, distance); end
+      def right(robot, distance)
+        robot.x += distance
+      end
 
-      def left(robot, distance); end
+      def left(robot, distance)
+        robot.x -= distance
+      end
 
-      def save(robot); end
+      def save(robot)
+        robot.save!
+      end
     end
   end
 end
