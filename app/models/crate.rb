8a2d5c9cf4485e5754e26c33c5f5cbde0c3114a4
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-04-23 15:44:25 +0100

    Validate that crates are put in available locations

--- app/models/crate.rb
+++ app/models/crate.rb
@@ -5,12 +5,22 @@ class Crate < ApplicationRecord
   belongs_to :warehouse
 
   validate :location_is_inside_boundaries
+  validate :location_is_available
   validates :warehouse, presence: true
+
+  scope :in_location, ->(x, y, warehouse) { where(x: x, y: y, warehouse: warehouse) }
+  scope :others_than, ->(crate) { where.not(id: crate.id) }
 
   def location_is_inside_boundaries
     error_location_outside = 'is not a positive location inside the warehouse'
 
     errors.add(:x, error_location_outside) unless x.between?(1, warehouse.width)
     errors.add(:y, error_location_outside) unless y.between?(1, warehouse.length)
+  end
+
+  def location_is_available
+    error_location = 'Location is not available for the crate'
+
+    errors[:base] << error_location if Crate.others_than(self).in_location(x, y, warehouse).exists?
   end
 end
