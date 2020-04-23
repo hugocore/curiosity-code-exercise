caf07cd81f3251ed5a2ed9a5e9f4d234df8d4d32
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-04-23 10:47:38 +0100

    Setup a Dry-Container registry

---
+++ app/domains/registry/container.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Registry
+  class Container
+    extend Dry::Container::Mixin
+  end
+end
---
+++ app/domains/registry/inject.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Registry
+  Inject = Dry::AutoInject(Container)
+end
