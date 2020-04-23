88f5b162b69682c0ab9489517a5a04653edf5006
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-04-23 22:00:45 +0100

    Test that Robot does not try to grab a crate if its lifting one

--- spec/domains/operational/grabbing_service_spec.rb
+++ spec/domains/operational/grabbing_service_spec.rb
@@ -5,8 +5,9 @@ require 'rails_helper'
 RSpec.describe Operational::GrabbingService do
   subject(:drop_service) { described_class.new(robots_repo: repo) }
 
-  let(:repo) { instance_double Repositories::Robots::Base, grab_crate: nil }
-  let(:robot) { create :robot }
+  let(:repo) { instance_double Repositories::Robots::Base, grab_crate: nil, holding_crate?: crate }
+  let(:robot) { build(:robot) }
+  let(:crate) { false }
 
   describe '#call' do
     before { drop_service.call(robot: robot) }
@@ -17,6 +18,14 @@ RSpec.describe Operational::GrabbingService do
 
     context 'without a robot' do
       let(:robot) { nil }
+
+      it 'does not call the robots repository' do
+        expect(repo).not_to have_received(:grab_crate).with(robot)
+      end
+    end
+
+    context 'with a robot already holding a crate' do
+      let(:crate) { true }
 
       it 'does not call the robots repository' do
         expect(repo).not_to have_received(:grab_crate).with(robot)
