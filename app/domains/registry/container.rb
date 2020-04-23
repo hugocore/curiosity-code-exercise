94df04e0ff3a0b9e5ccffe4f77a3975bc53c66cc
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-04-23 14:09:55 +0100

    Remove registry from the domain

--- app/domains/registry/container.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Registry
-  class Container
-    extend Dry::Container::Mixin
-
-    register(:robots_repo) { Repositories::Robots::ActiveRecordAdaptor.new }
-  end
-end
--- app/domains/registry/inject.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Registry
-  Inject = Dry::AutoInject(Container)
-end
